4ba26c4da9cf988431d987c19c6868d2ea1a7f56
Qiyuan Yang <41722507+Qiyuan-Yang@users.noreply.github.com>
2018-10-12 16:41:52 +0800

    little change

--- touhouCDspaider/touhouCDspaider/touhouCDspaider.py
+++ touhouCDspaider/touhouCDspaider/touhouCDspaider.py
@@ -4,7 +4,7 @@ import bs4
 import re
 import time
  
-def Gethtmltext(url, code="utf-8"): 
+def getHtmlText(url, code="utf-8"): 
     #a simple modul to get html text
     try:
         r = requests.get(url,headers = {'user-agent':'Mozilla/5.0'})
@@ -14,26 +14,26 @@ def Gethtmltext(url, code="utf-8"):
     except:
         return ''
 
-def Getalbumhtml(url,startalbum = '"Activity" Case：01 -Graveyard Memory-'): 
+def getAlbumHtml(url,startAlbum = '"Activity" Case：01 -Graveyard Memory-'): 
     #to get the html text in album pages
-    html = Gethtmltext(url+startalbum)
+    html = getHtmlText(url+startAlbum)
     soup = BeautifulSoup(html, 'html.parser')
     for tag in soup.find_all('div'):
         if tag.get('class') == ['mw-category-generated']:
             return(tag)
 
-def Generatealbumlist(albumlist,startalbum = '"Activity" Case：01 -Graveyard Memory-'):
+def generateAlbumList(albumList,startAlbum = '"Activity" Case：01 -Graveyard Memory-'):
     #to get album names in album pages
-    for title in Getalbumhtml(url,startalbum).find_all('a'):
+    for title in getAlbumHtml(url,startAlbum).find_all('a'):
         if title.string == '上一页' or title.string == '下一页':
             continue
         else:
-            albumlist.append(title.string)
+            albumList.append(title.string)
 
-def Exportalbuminfo(albumlist,startalbum = '"Activity" Case：01 -Graveyard Memory-'):
+def exportAlbumInfo(albumList,startAlbum = '"Activity" Case：01 -Graveyard Memory-'):
     #actually the main function
     mode = eval(input('模式1 为 获取专辑列表 模式2 为 获取单曲列表\n'))
-    Generatealbumlist(albumlist,startalbum)
+    generateAlbumList(albumList,startAlbum)
     f = open('C:/localdata.txt','w',encoding='utf-8')
     number = 0
     #an additional modul to make a prograss bar
@@ -42,7 +42,7 @@ def Exportalbuminfo(albumlist,startalbum = '"Activity" Case：01 -Graveyard Memo
     start = time.perf_counter()
     print("执行开始".center(scale//2, "-"))
     #try to traverse all album pages
-    while len(albumlist) > 1:
+    while len(albumList) > 1:
         a = '*' * int(count/1.1)
         b = '.' * (scale - int(count/1.1))
         c = (int(count/1.1)/scale)*100
@@ -50,16 +50,16 @@ def Exportalbuminfo(albumlist,startalbum = '"Activity" Case：01 -Graveyard Memo
         print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c,a,b,dur),end='')
         count += 1
         #try to get all vocal/music in each album
-        for i in range(len(albumlist)):
-            if i != 0 or startalbum == '"Activity" Case：01 -Graveyard Memory-':
+        for i in range(len(albumList)):
+            if i != 0 or startAlbum == '"Activity" Case：01 -Graveyard Memory-':
                 if mode == 1:
-                    f.write(albumlist[i])
+                    f.write(albumList[i])
                     f.write('\n')
                 else:
-                    Getalbuminfo(albumlist[i],f)
-        startalbum = albumlist[-1]
-        albumlist = []
-        Generatealbumlist(albumlist,startalbum)
+                    getAlbumInfo(albumList[i],f)
+        startAlbum = albumList[-1]
+        albumList = []
+        generateAlbumList(albumList,startAlbum)
     f.close()
     #the additional modul of prograss bar to make sure it stop at 100%
     a = '*' * 50
@@ -69,15 +69,15 @@ def Exportalbuminfo(albumlist,startalbum = '"Activity" Case：01 -Graveyard Memo
     print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c,a,b,dur),end='')
     print("\n"+"执行结束".center(scale//2,'-'))
 
-def Getalbuminfo(albumname,filename):
+def getAlbumInfo(albumName,fileName):
     #actually the most usful function
     #it contain the function to get all vocal/music and the function to make a output
-    albumurl = 'https://thwiki.cc/'
-    html = Gethtmltext(albumurl+albumname)
+    albumUrl = 'https://thwiki.cc/'
+    html = getHtmlText(albumUrl+albumName)
     soup = BeautifulSoup(html, 'html.parser')
     text = []
-    musicinfo = []
-    musicinfo.append(albumname)
+    musicInfo = []
+    musicInfo.append(albumName)
     for tag in soup.find_all('table'):
         if tag.get('class') == ['wikitable', 'musicTable']:
             for item in tag.find_all('tr'):
@@ -87,16 +87,16 @@ def Getalbuminfo(albumname,filename):
                         if item.a.string == None:
                             for j in item.find_all('td'):
                                 pattern = '>(.*?)<span'
-                                musicname = re.search(pattern,str(j))
-                                if musicname:
-                                    musicname = musicname.group(0)[1:-5]
-                                    if '\u3000' in musicname:
-                                        musicname = musicname.replace('\u3000',' ')
-                                    musicinfo.append(musicname)
+                                musicName = re.search(pattern,str(j))
+                                if musicName:
+                                    musicName = musicName.group(0)[1:-5]
+                                    if '\u3000' in musicName:
+                                        musicName = musicName.replace('\u3000',' ')
+                                    musicInfo.append(musicName)
                         else:
                             if '\u3000' in item.a.string:
                                 item.a.string = item.a.string.replace('\u3000',' ')
-                            musicinfo.append(item.a.string)
+                            musicInfo.append(item.a.string)
                 except AttributeError:
                     for td in item.find_all('td'):
                         if td.string == '原曲':
@@ -104,80 +104,65 @@ def Getalbuminfo(albumname,filename):
                                 if ogmusic.string != None:
                                     if '\u3000' in ogmusic.string:
                                         ogmusic.string = ogmusic.string.replace('\u3000',' ')
-                                        musicinfo.append(ogmusic.string)
+                                        musicInfo.append(ogmusic.string)
                                     else:
-                                        musicinfo.append(ogmusic.string)
+                                        musicInfo.append(ogmusic.string)
                         else:
                             continue
-                        if musicinfo[-1] != albumname and musicinfo[-2] != albumname:
-                            for position in range(len(musicinfo)):
-                                filename.write(musicinfo[position])
-                                if position == len(musicinfo) - 1:
-                                    filename.write('\n')
+                        if musicInfo[-1] != albumName and musicInfo[-2] != albumName:
+                            for position in range(len(musicInfo)):
+                                fileName.write(musicInfo[position])
+                                if position == len(musicInfo) - 1:
+                                    fileName.write('\n')
                                 else:
-                                    filename.write(',')
-                            musicinfo = []
-                            musicinfo.append(albumname)
+                                    fileName.write(',')
+                            musicInfo = []
+                            musicInfo.append(albumName)
                         else:
                             continue
+
+def ogmusicMatch(file,ogmusic,lang = '中文'):
+    count = 0
+    f = open(file,encoding = 'UTF-8')
+    for line in f.readlines():
+        info = line.split(',')
+        if ogmusic in info:
+            count += 1
+            if lang == '中文':
+                print('第{}首'.format(count),end = ':')
+                print('\n专辑名：{} \n单曲名：{}'.format(info[0],info[1]))
+            elif lang == 'English':
+                print('The No.{}'.format(count),end = ':')
+                print('\nalbum name:{} \nsingle name:{}'.format(info[0],info[1]))
+
+def getSingleInfo(file,single,lang = '中文'):
+    f = open(file,encoding = 'UTF-8')
+    for line in f.readlines():
+        info = line.split(',')
+        if single in info[1]:
+            if lang == '中文':
+                print('所属专辑：{} \n包含原曲及原曲来源：{}'.format(info[0],info[1:-1]))
+            elif lang == 'English':
+                print('the album it belongs to：{} \nthe ogmusic it contains：{}'.format(info[0],info[1:-1]))
+
+def getAlbumInfo(file,album,lang):
+    f = open(file,encoding = 'UTF-8')
+    for line in f.readlines():
+        info = line.split(',')
+        if single in info[0]:
+            if lang == '中文':
+                print('包含单曲：{} \n原曲及原曲来源：{}'.format(info[1],info[1:-1]))
+            elif lang == 'English':
+                print('the singles it contains：{} \nthe ogmusic it contains：{}'.format(info[1],info[1:-1]))
+
+def main():
 
 url = 'https://thwiki.cc/index.php?title=分类:同人专辑&pagefrom='
 albumlist = []
 choice = input('是否定义起始专辑\n')
 if choice == 'yes':
     startalbum = input('输入起始专辑\n')
-    Exportalbuminfo(albumlist,startalbum)
+    exportAlbumInfo(albumlist,startalbum)
 else:
-    Exportalbuminfo(albumlist)
+    exportAlbumInfo(albumlist)
 
-'''
-version2：
-i = 0
-while i != len(text):
-    try:
-        item = text[i]
-        if item.a.string == None:
-            for j in item.find_all('td'):
-                pattern = '>(.*?)<span'
-                musicname = re.search(pattern,str(j))
-                if musicname:
-                    print(musicname.group(0)[1:-5])
-            i += 3
-            for ogmusic in text[i].find_all('a'):
-                if ogmusic.string != None:
-                    print(ogmusic.string)
-            i += 1
-        else:
-            print(item.a.string)
-            i += 5
-            for ogmusic in text[i].find_all('a'):
-                if ogmusic.string != None:
-                    print(ogmusic.string)
-            i += 1
-    except:
-        continue
-version1：
-            try:
-                num = item.b.string
-                if num in '0102030405060708091011213141516171819':
-                    if item.a.string == None:
-                        for i in item.find_all('td'):
-                            pattern = '>(.*?)<span'
-                            musicname = re.search(pattern,str(i))
-                            if musicname:
-                                print(musicname.group(0)[1:-5])
-                    else:
-                        print(item.a.string)
-            except:
-                continue
-            try:
-                if item.b.string != None:
-                    for i in item.find_all('td'):
-                        pattern = '>(.*?)<span'
-                        musicname = re.search(pattern,str(i))
-                        if musicname:
-                            print(musicname.group(0)[1:-5])
-                        print('/n')
-            except:
-                continue
-'''
